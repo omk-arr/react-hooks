ff9c425436e14466c56df061d73fa4e572519a80
omk-arr <omi.s.pawar@gmail.com>
2025-06-02 02:45:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import UseState from "./components/UseState";
+export function App(props) {
+  // useState Example
+  return <UseState />;
+}
+export default App;
---
+++ src/components/UseState.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+function UseState(props) {
+  const [counter, setCounter] = useState(0);
+  counterOp = (val) => {
+    if (val === 1) setCounter(counter + 1);
+    else if (val === 0 && counter > 0) setCounter(counter - 1);
+    else setCounter(0);
+  };
+  return (
+    <>
+      <h5>Counter {counter}</h5>
+      <button onClick={() => counterOp(1)}>Increment</button>
+      <button onClick={() => counterOp(0)}>Decrement</button>
+      <button onClick={() => counterOp(2)}>Reset</button>
+    </>
+  );
+}
+export default UseState;
